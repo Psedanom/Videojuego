bdb3b8399da3c265614f741830467ebe54a7197e
emiali2300-cpu <emiali2300@gmail.com>
2026-03-27 18:06:09 -0600

    clases de cartas y base del juego

--- DeadDraw/juegopro.js
+++ DeadDraw/juegopro.js
@@ -58,12 +58,16 @@ class HealthBar{
                          (this.y),
                          this.width,
                          this.height);
-        let healthli = (this.maxHealth/this.health)*this.width;
+        let healthli = (this.health/this.maxHealth)*this.width;
         ctx.fillStyle = "red";
             ctx.fillRect((this.x),
                          (this.y +1),
                          healthli,
                          this.height -2);
+        ctx.fillStyle = "white";
+            ctx.font = "20px Arial";
+            ctx.textAlign = "center";
+            ctx.fillText(this.health, this.width -35, this.height +12);
     }
 }
 class Botones{
@@ -85,7 +89,7 @@ class Botones{
     }
 }
 
-class cards {
+class cards{
     constructor(x, y, width, height, number, type,scale,used,inboard,enMazo) {
         this.x = x;
         this.y = y;
@@ -131,10 +135,10 @@ class cards {
     contains(mx, my) {
         return mx >= this.x && mx <= this.x + this.width && my >= this.y && my <= this.y + this.height;
     }
-    /*
+    
     defx(x){
         this.x = x;
-    }*/
+    }
     update(){
         if (this.isHovered) {
             this.scale = 1.2;
@@ -150,40 +154,123 @@ class cards {
     }
 
 }
+
+class CardEnemie extends cards{
+    draw(ctx) {
+            ctx.fillStyle = "black";
+            ctx.fillRect(this.x,
+                         this.y,
+                         this.width * this.scale,
+                         this.height* this.scale);
+        ctx.fillStyle = "white";
+            ctx.font = "20px Arial";
+            ctx.textAlign = "center";
+            ctx.fillText(this.number, this.x +20, this.y+20);
+    }
+    actionUse(health){
+        health-=this.number;
+        return health;
+    }
+    actionWeapon(health,num){
+        this.daño = this.number -num;
+        if(this.daño <0){
+            return health;
+        }
+        health -= this.daño;
+        return health;
+    }
+    arma(){
+        return false;
+    }
+    enemie(){
+        return true;
+    }
+}
+class CardVida extends cards{
+    draw(ctx) {
+            ctx.fillStyle = "red";
+            ctx.fillRect(this.x,
+                         this.y,
+                         this.width * this.scale,
+                         this.height* this.scale);
+        ctx.fillStyle = "white";
+            ctx.font = "20px Arial";
+            ctx.textAlign = "center";
+            ctx.fillText(this.number, this.x +20, this.y+20);
+    }
+    actionUse(health){
+        if(health < 20){
+            if(health + this.number > 20){
+                health = 20;
+            }
+            else{
+                health += this.number;
+            }
+        }
+        return health;
+    }
+    arma(){
+        return false;
+    }
+}
+
+class CardEspada extends cards{
+    draw(ctx) {
+            ctx.fillStyle = "orange";
+            ctx.fillRect(this.x,
+                         this.y,
+                         this.width * this.scale,
+                         this.height* this.scale);
+        ctx.fillStyle = "white";
+            ctx.font = "20px Arial";
+            ctx.textAlign = "center";
+            ctx.fillText(this.number, this.x +20, this.y+20);
+    }
+    recallNum(){
+        return this.number;
+    }
+    arma(){
+        return true;
+    }
+}
+
 class Game{
     constructor(canvas) {
-        this.cards = [];
+        this.cartas = [];
         this.createEventListeners();
         this.initObjects();
         this.canvas = canvas;
         this.clicked = false;
         this.tablaVacia = false;
+        this.cnt = 0;
         this.ctab = 4;
         this.cantidadCartasTablero = 4;
+        this.cartasArma = [];
+        this.hayArma = false;
     }
 
     initObjects(){
-        for (let i = 0; i < 10;i++){
-            let card = new cards(0, 200, 112.5, 150,10, "diamantes",1,false,false,true);
-            this.cards.push(card);
+        for (let i = 1; i <11;i++){
+            let card = new CardEspada(0, 200, 112.5, 150,i, "diamantes",1,false,false,true);
+            this.cartas.push(card);
         }
-        for (let i = 0; i < 10;i++){
-            let card = new cards(0, 200, 112.5, 150, Math.floor(Math.random() * (10 - 1) + 1), "treboles",1,false,false,true);
-            this.cards.push(card);
+        for (let i = 1; i <11;i++){
+            let card = new CardEnemie(0, 200, 112.5, 150, i, "treboles",1,false,false,true);
+            this.cartas.push(card);
         }
-        for (let i = 0; i < 10;i++){
-            let card = new cards(0, 200, 112.5, 150, Math.floor(Math.random() * (10 - 1) + 1), "espadas",1,false,false,true);
-            this.cards.push(card);
+        for (let i = 1; i <11;i++){
+            let card = new CardEnemie(0, 200, 112.5, 150, i, "espadas",1,false,false,true);
+            this.cartas.push(card);
         }
-        for (let i = 0; i < 10;i++){
-            let card = new cards(0, 200, 112.5, 150, Math.floor(Math.random() * (10 - 1) + 1), "corazones",1,false,false,true);
-            this.cards.push(card);
+        for (let i = 1; i <11;i++){
+            let card = new CardVida(0, 200, 112.5, 150, i, "corazones",1,false,false,true);
+            this.cartas.push(card);
         }
         this.contador = new tiempo();
         this.armas = new Botones(100,470,120,170);
         this.usadas = new Botones(650,400,120,170);
         this.playerHealth = new HealthBar(15,15,100,20,20);
-        shuffle(this.cards); 
+        shuffle(this.cartas); 
         
     }
     createEventListeners() {
@@ -191,26 +278,55 @@ class Game{
             const rect = this.canvas.getBoundingClientRect();
             const mouseX = event.clientX - rect.left;
             const mouseY = event.clientY - rect.top;
-            for (let card of this.cards) {
+            for (let card of this.cartas) {
                 card.isHovered = card.contains(mouseX, mouseY);
             }
             this.armas.isHovered = this.armas.tocando(mouseX,mouseY);
             this.usadas.isHovered = this.usadas.tocando(mouseX,mouseY);
         });
         canvas.addEventListener('click', (event) =>{
-            for(let card of this.cards){
-                if(card.isHovered && !this.clicked && !card.used){
+            for(let card of this.cartas){
+                if(card.isHovered  && !card.used){
                     this.clicked = true;
                     this.card_clicked = card;
                     break;
                 }
                 else if(this.armas.isHovered && this.clicked){
-                    this.xar = this.armas.x;
-                    this.yar = this.armas.y;
-                    this.card_clicked.click(this.xar,this.yar);
-                    this.clicked = false;
-                    this.card_clicked.inboard = false;
-                    this.ctab -=1;
+                    if(this.card_clicked.arma()){
+                        if(this.hayArma){
+                            this.clicked = false;
+                            this.card_clicked.used = false;
+                        }
+                        else{
+                            this.card_clicked.used = true;
+                            this.cartasArma.push(this.card_clicked);
+                            this.xar = this.armas.x;
+                            this.yar = this.armas.y;
+                            this.card_clicked.click(this.xar,this.yar);
+                            this.clicked = false;
+                            this.card_clicked.inboard = false;
+                            this.ctab -=1;
+                            this.hayArma = true;
+                        }
+                    }
+                    else if(this.hayArma && this.card_clicked.enemie()){
+                        this.card_clicked.used = true;
+                        this.cartasArma.push(this.card_clicked);
+                        for(let cartasrma of this.cartasArma){
+                            if(cartasrma.arma()){
+                                this.numberArma = cartasrma.number;
+                            }
+                        }
+                        this.playerHealth.health = this.card_clicked.actionWeapon(this.playerHealth.health,this.numberArma);
+                        this.xar = this.armas.x;
+                        this.yar = this.armas.y;
+                        this.card_clicked.click(this.xar,this.yar);
+                        this.clicked = false;
+                        this.card_clicked.inboard = false;
+                        this.ctab -=1;
+                    }
+                    console.log(this.hayArma);
+
                     break;
                 }
                 else if(this.usadas.isHovered && this.clicked){
@@ -220,6 +336,7 @@ class Game{
                     this.clicked = false;
                     this.card_clicked.inboard = false;
                     this.ctab -=1;
+                    this.playerHealth.health = this.card_clicked.actionUse(this.playerHealth.health);
                     break;
                 }
                     
@@ -229,33 +346,28 @@ class Game{
     update(deltaTime){
 
         if(this.ctab <= 1){
-            for(let card of this.cards ){
+            for(let card of this.cartas ){
                 if(!card.used && card.inboard){
                     card.x = 100;
                     this.tablaVacia = true;
-                     
                 }
             }
+            terminado = false;
             this.ctab = 4;
         }
-        for(let card of this.cards ){
+        for(let card of this.cartas ){
             card.update();
         }
         this.contador.contador(deltaTime);
-    } 
-    draw(ctx){
-        this.armas.draw(ctx);
-        this.usadas.draw(ctx);
-        this.playerHealth.draw(ctx);
+
         this.num = 0;
         let posicion = 100;
         if(this.tablaVacia && !terminado){
             this.cantidadCartasTablero += 3
             posicion = 262.5
-            this.tablaVacia = false
         }
         
-        for(let card of this.cards){
+        for(let card of this.cartas){
             if(this.num < this.cantidadCartasTablero){
                 if(!card.used && !card.inboard){
                     card.x = posicion;
@@ -268,6 +380,12 @@ class Game{
          }
         this.tablaVacia = false;
         terminado = true;
+    } 
+    draw(ctx){
+        this.armas.draw(ctx);
+        this.usadas.draw(ctx);
+        this.playerHealth.draw(ctx);
+        
         this.contador.draw(ctx);
     }
 }
