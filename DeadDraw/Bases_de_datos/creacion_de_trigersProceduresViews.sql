6a8fba35d2fbedadd61d71ad9caac4c92be52383
Psedanom <psedanom@gmail.com>
2026-04-22 22:33:33 -0600

    Arreglos de base de datos, cardinalidades ademas de que se subio el diagrama

--- DeadDraw/Bases_de_datos/creacion_de_trigersProceduresViews.sql
+++ DeadDraw/Bases_de_datos/creacion_de_trigersProceduresViews.sql
@@ -66,23 +66,30 @@ SELECT username,sum(result = 'victory') as times_wined FROM player INNER JOIN ma
 CREATE VIEW deck_card_types AS
 select name,suit, count(*) as count from deck INNER JOIN cardindeck USING(idDeck) INNER JOIN card USING(idCard) group by name, suit;
 
+
+
+/* -------------------------------- triggers -------------------------------- */
+
 /*this trigger is used to update the score of the player after a match*/
+drop trigger if exists update_score_after_match;
 DELIMITER $$
-CREATE Trigger update_score_after_match
-AFTER UPDATE ON matchgame
+CREATE TRIGGER update_score_after_match
+BEFORE UPDATE ON matchgame
 FOR EACH ROW
 BEGIN
     IF NEW.result = 'victory' THEN
-        UPDATE player SET score = score + 1 WHERE idPlayer = NEW.idPlayer;
+        SET NEW.score = NEW.score + 1;
     END IF;
 END$$
 DELIMITER ;
+
 /*this trigger is used to not allow the player to have a password that is less than 8 characters long
 */
 DELIMITER $$
 CREATE Trigger INSERT_player_passwordTooShort
 BEFORE INSERT ON player
-FOR EACH ROWBEGIN
+FOR EACH ROW
+BEGIN
     IF LENGTH(NEW.password) < 8 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Password must be at least 8 characters long';
     END IF;
@@ -91,19 +98,23 @@ DELIMITER ;
 
 /*this trigger is used to update the lastUpdate field of the player table every time the player updates his information
 */
+
+DROP TRIGGER IF EXISTS update_lastUpdate;
 DELIMITER $$
 CREATE Trigger update_lastUpdate
-AFTER UPDATE ON player
+BEFORE UPDATE ON player
 FOR EACH ROW
 BEGIN
-    UPDATE player SET lastUpdate = NOW() WHERE idPlayer = NEW.idPlayer;
+    SET NEW.lastUpdate = NOW();
 END$$
 DELIMITER ;
+
+DROP TRIGGER IF EXISTS update_username_duplicate;
 /*this trigger is used to not allow the player to have a username that already exists in the database
 */
 DELIMITER $$
 CREATE Trigger update_username_duplicate
-BEFORE UPDATE ON player
+BEFORE INSERT ON player
 FOR EACH ROW
 BEGIN
     IF EXISTS (SELECT * FROM player WHERE username = NEW.username AND idPlayer != NEW.idPlayer) THEN
@@ -111,12 +122,17 @@ BEGIN
     END IF;
 END$$
 DELIMITER ;
+
+/* ------------------------------------ procedure---------------------------------- */
+
+
+DROP PROCEDURE IF EXISTS player_cardsPowerups;
 /*this procedure is used to get the cards that the player has with effect and in wich maze the player has them
 */
 DELIMITER $$
 CREATE Procedure player_cardsPowerups(IN p_idPlayer INT)
 BEGIN
-    SELECT username, name, suit, e.name as effect_name
+    SELECT username, d.name, suit, e.name as effect_name
     FROM player
     INNER JOIN matchgame USING (idPlayer)
     INNER JOIN Deck d USING (idDeck)
@@ -128,6 +144,9 @@ BEGIN
     ORDER BY idDeck DESC;
 END$$
 DELIMITER ;
+
+
+
 /*this procedure is used to get the lootboxes that the player has opened and their cost and quality and opening date
 */
 DELIMITER $$
@@ -141,22 +160,27 @@ BEGIN
     ORDER BY openingDate DESC;
 END$$
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS all_players_scores;
 /*this procedure is used to get the avg score of all the players and the total number of players in the database
 */
 DELIMITER $$
 CREATE Procedure all_players_scores(out total_players INT, out average_score FLOAT)
 BEGIN
     SELECT COUNT(*) INTO total_players FROM player;
-    SELECT AVG(score) INTO average_score FROM player;
+    SELECT ROUND(AVG(score)) INTO average_score FROM matchgame;
 END$$
 
+
 DELIMITER ;
+DROP PROCEDURE IF EXISTS deck_usage_statistics;
 /*this procedure is used to get the number of decks in the database and the average usage of each deck in the matches
 */
 DELIMITER $$
 CREATE Procedure deck_usage_statistics(out total_decks INT, out average_usage FLOAT)
 BEGIN
     SELECT COUNT(*) INTO total_decks FROM deck;
-    SELECT AVG(idDeck) INTO average_usage FROM matchgame;
+    SELECT round(AVG(idDeck)) INTO average_usage FROM matchgame;
 END$$
 delimiter ;
+
